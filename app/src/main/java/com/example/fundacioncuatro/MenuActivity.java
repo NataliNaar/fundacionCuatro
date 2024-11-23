d9b8695b5bb7c75e5b772655a5ffceff936de41e
natiinaar <nnaardelabarrera@gmail.com>
2024-11-23 14:42:46 -0500

    Haciendo CRUD de registro niños

--- app/src/main/java/com/example/fundacioncuatro/MenuActivity.java
+++ app/src/main/java/com/example/fundacioncuatro/MenuActivity.java
@@ -1,6 +1,9 @@
 package com.example.fundacioncuatro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MenuActivity extends AppCompatActivity {
+    Button btnChildren, btnAdult , btnDonate, btnVoluntary, btnBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,48 @@ public class MenuActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnChildren = findViewById(R.id.btnRegistrationChildren);
+        btnAdult = findViewById(R.id.btnRegistrationAdult);
+        btnDonate = findViewById(R.id.btnRegistrationDonate);
+        btnVoluntary = findViewById(R.id.btnRegistrationVoluntary);
+        btnBack = findViewById(R.id.btnBack);
+
+
+        //Botón Registro de niños
+        btnChildren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, RegChildrenActivity.class);
+            }
+        });
+
+        //Botón Registro de acudientes
+        btnAdult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, RegAdultActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Botón Registro de Donates
+        btnDonate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, RegEventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Acción para el botón "Regresar"
+        //Botón "Regresar" ( btnBack): Cuando se hace clic en este botón, se llama al método onBackPressed(), que retrocede a la actividad anterior. Esto simula la acción de presionar el botón físico "Atrás" en Android.
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/fundacioncuatro/RegChildrenActivity.java
@@ -0,0 +1,119 @@
+package com.example.fundacioncuatro;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegChildrenActivity extends AppCompatActivity {
+
+     EditText etNumeroIdentificacion, etNombre, etApellidos, etEdad;
+     Button btnEditar, btnActualizar, btnEliminar, btnRegresar;
+
+     FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_reg_children);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+     db = FirebaseFirestore.getInstance();
+
+        etNumeroIdentificacion = findViewById(R.id.etNumeroIdentificacion);
+        etNombre = findViewById(R.id.etNombre);
+        etApellidos = findViewById(R.id.etApellidos);
+        etEdad = findViewById(R.id.etEdad);
+        btnEditar = findViewById(R.id.btnEditar);
+        btnActualizar = findViewById(R.id.btnActualizar);
+        btnEliminar = findViewById(R.id.btnEliminar);
+        btnRegresar = findViewById(R.id.btnRegresar);
+
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardarDatos());
+            }
+        });
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editarDatos());
+            }
+        });
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eliminarDatos());
+            }
+        });
+    }
+    private void guardarDatos() {
+        String id = etNumeroIdentificacion.getText().toString();
+        String nombre = etNombre.getText().toString();
+        String apellidos = etApellidos.getText().toString();
+        String edad = etEdad.getText().toString();
+
+        if (id.isEmpty() || nombre.isEmpty() || apellidos.isEmpty() || edad.isEmpty()) {
+            // Muestra un mensaje de error si algún campo está vacío
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Crear un mapa de datos
+        Map<String, Object> niño = new HashMap<>();
+        niño.put("numeroIdentificacion", id);
+        niño.put("nombre", nombre);
+        niño.put("apellidos", apellidos);
+        niño.put("edad", edad);
+
+        // Guardar en Firestore
+        db.collection("niños").document(id)
+                .set(niño)
+                .addOnSuccessListener(aVoid -> Toast.makeText(this, "Datos guardados", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(this, "Error al guardar", Toast.LENGTH_SHORT).show());
+    }
+    private void editarDatos() {
+        String id = etNumeroIdentificacion.getText().toString();
+
+        db.collection("niños").document(id)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        etNombre.setText(documentSnapshot.getString("nombre"));
+                        etApellidos.setText(documentSnapshot.getString("apellidos"));
+                        etEdad.setText(documentSnapshot.getString("edad"));
+                    } else {
+                        Toast.makeText(this, "No se encontraron datos", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(e -> Toast.makeText(this, "Error al obtener datos", Toast.LENGTH_SHORT).show());
+    }
+    private void eliminarDatos() {
+        String id = etNumeroIdentificacion.getText().toString();
+
+        db.collection("niños").document(id)
+                .delete()
+                .addOnSuccessListener(aVoid -> Toast.makeText(this, "Datos eliminados", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(this, "Error al eliminar", Toast.LENGTH_SHORT).show());
+    }
+
+
+}
