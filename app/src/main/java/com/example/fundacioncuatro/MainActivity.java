d5c42365868b181895bee7c84783d4d7e9849864
natiinaar <nnaardelabarrera@gmail.com>
2024-11-23 12:42:18 -0500

    Logica de login

--- app/src/main/java/com/example/fundacioncuatro/MainActivity.java
+++ app/src/main/java/com/example/fundacioncuatro/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.fundacioncuatro;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+     EditText emailEditText, passwordEditText;
+     Button loginButton;
+     FirebaseAuth mAuth;
+    TextView tvMessageR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,51 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Inicia Firebase Auth
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
+
+
+        //Lógica para inicio de sesión
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (!email.isEmpty() && !password.isEmpty()){
+                    mAuth
+                            .signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                                        startActivity(intent);
+                                    }
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    tvMessageR.setTextColor(Color.parseColor("#800000"));
+                                    tvMessageR.setText("No se creo la cuenta");
+                                }
+                            });
+
+                } else{
+                    tvMessageR.setTextColor(Color.parseColor("#800000"));
+                    tvMessageR.setText("Debe ingresar todos los datos");
+                }
+
+            }
         });
     }
 }
